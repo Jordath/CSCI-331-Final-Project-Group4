589d3597ca9df8a438abb5f13ddcc2d1de81a7ed
Jordath <49654668+Jordath@users.noreply.github.com>
2021-11-30 17:27:38 -0700

    f

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,5 @@
 import app from "./server.js"
+
 import mongodb from "mongodb"
 import dotenv from "dotenv"
 import recipesDAO from "./dao/recipesDAO.js"
@@ -6,10 +7,12 @@ dotenv.config()
 const MongoClient = mongodb.MongoClient
 
 const port = process.env.port || 8000
+RESTREVIEWS_DB_URI='mongodb+srv://BMarceau:WinterIsComing1@cluster0.1i945.mongodb.net/recDB?retryWrites=true&w=majority'
 
 if(process.env.NODE_ENV === 'production'){
     app.use(express.static('frontend\build'));
 }
+
 
 MongoClient.connect(
     process.env.RESTREVIEWS_DB_URI,
